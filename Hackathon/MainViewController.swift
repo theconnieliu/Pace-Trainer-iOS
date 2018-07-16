5f9785257e77e4db6ee5664a84518a63249bf3a6
theconnieliu <connieliu73@gmail.com>
2018-07-16 16:38:49 -0700

    Set view constraints

--- Hackathon/MainViewController.swift
+++ Hackathon/MainViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    
+    var hour : Int = 0
+    var minutes : Int = 0
+    var seconds : Int = 0
 
+    @IBOutlet weak var targetTimePicker: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -32,4 +38,50 @@ class MainViewController: UIViewController {
     }
     */
 
+}
+
+extension MainViewController:UIPickerViewDelegate,UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 3
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        switch component {
+        case 0:
+            return 25
+        case 1,2:
+            return 60
+
+        default:
+            return 0
+        }
+    }
+
+    func pickerView(_ pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
+        return pickerView.frame.size.width/3
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        switch component {
+        case 0:
+            return "\(row) Hour"
+        case 1:
+            return "\(row) Minute"
+        case 2:
+            return "\(row) Second"
+        default:
+            return ""
+        }
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        switch component {
+        case 0:
+            hour = row
+        case 1:
+            minutes = row
+        case 2:
+            seconds = row
+        default:
+            break;
+        }
+    }
 }

45c3e6cb6b79b2071c0cdbb315f6e45dbeb79811
theconnieliu <connieliu73@gmail.com>
2018-07-16 13:16:54 -0700

    Created new repository with initial UI UX

---
+++ Hackathon/MainViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MainViewController.swift
+//  Hackathon
+//
+//  Created by Tania Ortiz-Rosales on 7/16/18.
+//  Copyright © 2018 Tania Ortiz-Rosales. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
